e976c526ea40508534d5a277cd9a98e20f24755e
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-10-02 23:46:41 +0700

    menentukan tahun kabisat

---
+++ Pemilihan2Percobaan103.java
@@ -0,0 +1,17 @@
+/**
+ * Pemilihan2Percobaan103
+ */
+import java.util.Scanner;
+ public class Pemilihan2Percobaan103 {
+
+    public static void main(String[] args) {
+        Scanner input03 = new Scanner(System.in);
+        System.out.print("Masukkan Tahun: ");
+        int tahun = input03.nextInt();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
