87b652d078c5394e596886bcf1c4688c41e3289e
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-10-03 08:08:55 +0700

    a

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
+            else
+            System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
